9ce2cb59f00f69e027fd816cb69c83a467838dc3
Andrew K <nscapital08@gmail.com>
2022-06-27 22:58:42 +0300

    fix: fix misspelling in readme file

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,21 +2,9 @@ import React from 'react';
 
 function ImagePopup ({card, onClose}) {
 
-  function handleEscClose(evt) {
-    if(evt.key === 'Escape') {
-    window.addEventListener("keydown", onClose)
-    console.log(evt.key);
-    }   
-    console.log(evt.key); 
-  }
-
-
- /* React.useEffect(() => {
-    handleEscClose();
-  })*/
-
+  
     return(
-      <div className={`popup image-popup ${card ? 'popup_opened' : ''}`} onKeyDown={handleEscClose} >
+      <div className={`popup image-popup ${card ? 'popup_opened' : ''}`} >
         <figure className="image-popup__container">
           <button className="popup__close-button image-popup__close-button" onClick={onClose}></button>
           <img className="image-popup__image" src={card ? card.link : ''} alt={card ? card.name : ''} />
